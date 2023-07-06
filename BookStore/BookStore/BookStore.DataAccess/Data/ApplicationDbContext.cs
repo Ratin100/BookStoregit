d23365ddcae428161ec9d8bdab9e02fddea09ce0
Ratin Dev Sadhon <ratindevsadhon10@gmail.com>
2023-07-06 17:36:22 +0600

    FirstCommit

---
+++ BookStore/BookStore/BookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.DataAcess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser> 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+   
+        }
+ 
+
+}
